10d59644cd8fd1b47e23489fa079ea367b6d0d4f
tannerkrewson <tannerkrewson@gmail.com>
2018-02-11 20:47:35 -0600

    Added copy link button for generated playlist

--- src/components/Generator.js
+++ src/components/Generator.js
@@ -1,6 +1,7 @@
 import React, {
 	Component
 } from 'react';
+import Clipboard from 'react-clipboard.js';
 
 const css = {
 	paddingTop: '16px',
@@ -16,6 +17,11 @@ const cssCenter = {
 const butGroup = {
 	textAlign: 'center',
 	paddingTop: '12px',
+}
+
+const butGroupGroup = {
+	display: 'flex',
+	justifyContent: 'center'
 }
 
 const but = {
@@ -36,9 +42,18 @@ class Generator extends Component {
 				<div style={css}>
 					<h3>Playlist created!</h3>
 					<p>You can find it at the top of your Spotify playlists.</p>
-					<div className="card">
-						<div className="card-body">
-							<a href={this.props.playlistResult}>{this.props.playlistResult}</a>
+					<div className="btn-toolbar" style={butGroupGroup} role="toolbar" aria-label="Toolbar with button groups">
+						<div className="btn-group mr-2" role="group">
+							<Clipboard
+								type="button"
+								className="btn btn-info"
+								data-clipboard-text={this.props.playlistResult}>Copy Link</Clipboard>
+						</div>
+						<div className="btn-group mr-2" role="group">
+							<button type="button" className="btn">fb</button>
+							<button type="button" className="btn">tw</button>
+							<button type="button" className="btn">ig</button>
+							<button type="button" className="btn">etc</button>
 						</div>
 					</div>
 					<div style={butGroup}>
